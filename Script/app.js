a3e26c1a61128d5a1c0dde9784ce07a9ccdf4c39
Daniel Merritt <daniel.mafou@gmail.com>
2023-04-03 16:36:12 -0400

    basic win/loss window added

--- Script/app.js
+++ Script/app.js
@@ -6,7 +6,10 @@
 let boardEl = document.getElementById('board')
 let scoreEl = document.getElementById('score')
 let resetEl = document.getElementById('reset')
+let messageEl = document.getElementById('message')
 let sqEl =document.querySelectorAll('.sq')
+let winLoseMessage = document.getElementById('win-loss-message')
+let playAgainEl = document.getElementById('play-again')
 
 /*----------constants----------*/
 let snake= {
@@ -37,6 +40,7 @@ let keyPress = document.addEventListener('keydown', (event) => {
 }, false)
     
 resetEl.addEventListener('click', init)
+playAgainEl.addEventListener('click', playAgain)
 /*----------Losing Positions----------*/
 let losingBlocks = ["0A", "0B", "0C", "0D", "0E", "0F", "0E", "0F", "0G", "0H", "0I", "0J", "0K", "0L", "0M", "0N", "0O", "0P", "17A", "17B", "17C", "17D", "17E", "17F", "17G", "17H", "17I", "17J", "17K", "17L", "17M", "17N", "17O", "17P","1@", "2@", "3@", "4@", "5@", "6@", "7@", "8@", "9@", "10@", "11@", "12@", "13@", "14@", "15@", "16@", "1Q", "2Q", "3Q", "4Q", "5Q", "5Q", "6Q", "7Q", "8Q", "9Q", "10Q", "11Q", "12Q", "13Q", "14Q", "15Q", "16Q"]
 /*----------functions----------*/
@@ -50,6 +54,10 @@ function init() {
   movementID = setInterval(snakeMovement, intervalNum) 
   newPiece()
   render()
+}
+function playAgain() {
+  init()
+  messageEl.className = 'hidden'
 }
 function render() {
   sqEl.forEach(function(sq) {
@@ -73,6 +81,7 @@ function render() {
   })
   checkWin()
   checkLoss()
+  renderMessage()
 }
 function newPiece () {
   let number = Math.floor(Math.random() * 16 + 1)
@@ -100,6 +109,16 @@ function checkWin(){
   }
 }
 
+function renderMessage(){
+  if (loss === true) {
+    messageEl.className = "visible"
+    winLoseMessage.innerText = "Game Over. But that's ok."
+  }
+  if (win === true) {
+    messageEl.className = "visible"
+    winLoseMessage = "You Win!!! I don't expect many to see this."
+  }
+}
 /*----------movement functions----------*/
 function up() {
   if (snake.direction !== "down") {
