aa7f536700af2f27290f6e764c9fbbbaed55a0cd
Daniel Merritt <daniel.mafou@gmail.com>
2023-04-06 10:14:04 -0400

    organize data structure

--- Script/app.js
+++ Script/app.js
@@ -1,6 +1,15 @@
-
+import { 
+  changeDirection,
+  snakeMovement,
+  snake,
+} from "./snake.js"
+export {
+  win,
+  loss,
+  piece,
+  render,
+}
 /*-----cached elements-----*/
-let boardEl = document.getElementById('board')
 let scoreEl = document.getElementById('score')
 let resetEl = document.getElementById('reset')
 let messageEl = document.getElementById('message')
@@ -11,11 +20,7 @@ let eatSound = new Audio("../Assets/Audio/eatSound.mp3")
 let bonk = new Audio("../Assets/Audio/bonkSound.mp3")
 
 /*----------constants----------*/
-let snake= {
-  direction: null,
-  nextDirection: null,
-  size: ["8H"]
-}
+
 let score = 0
 let win = false
 let loss = false 
@@ -25,7 +30,7 @@ let movementID
 
 scoreEl.innerHTML= `Score: ${score}`
 /*---------event listeners----*/
-let keyPress = document.addEventListener('keydown', (event) => {
+document.addEventListener('keydown', (event) => {
   if(["ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(event.code) > -1) {
     event.preventDefault();
   }
@@ -135,70 +140,4 @@ function eatingSound() {
 function deathCry() {
   bonk.volume = .25
   bonk.play()
-}
-/*----------movement functions----------*/
-function up() {
-  if (snake.direction !== "down") {
-    snake.nextDirection = "up"
-  }
-}
-function down() {
-  if (snake.direction !== "up") {
-    snake.nextDirection = "down"
-  }
-}
-function left() {
-  if (snake.direction !== "right") {
-    snake.nextDirection = "left"
-  }
-}
-function right() {
-  if (snake.direction !== "left") {
-    snake.nextDirection = "right"
-  }
-}
-function changeDirection(key) {
-  if (key === "ArrowUp") {
-    up()
-  } else if (key === "ArrowDown") {
-    down()
-  } else if (key === "ArrowLeft") {
-    left()
-  } else if (key === "ArrowRight") {
-    right()
-  }
-}
-
-
-function snakeMovement(){
-  if(loss === true || win === true) {
-    clearInterval(movementID)
-    return
-  }
-  snake.direction = snake.nextDirection
-  if (snake.direction === "up") {
-    let letter = snake.size[0].slice(-1)
-    let newLetter = String.fromCharCode(letter.codePointAt(0) - 1)
-    let newSquare = snake.size[0].replace(letter, newLetter)
-    snake.size.unshift(newSquare)
-  } else if (snake.direction === "down"){
-    let letter = snake.size[0].slice(-1)
-    let newLetter = String.fromCharCode(letter.codePointAt(0) + 1)
-    let newSquare = snake.size[0].replace(letter, newLetter)
-    snake.size.unshift(newSquare)
-  } else if (snake.direction === "left"){
-    let number = snake.size[0].slice(0,-1)
-    let newNumber = parseInt(number) - 1
-    let newSquare = snake.size[0].replace(number, newNumber)
-    snake.size.unshift(newSquare)
-  } else if (snake.direction === "right") {
-    let number = snake.size[0].slice(0,-1)
-    let newNumber = parseInt(number) + 1
-    let newSquare = snake.size[0].replace(number, newNumber)
-    snake.size.unshift(newSquare)
-  }
-  if ((snake.size[0] !== piece) && (snake.direction !== null)) {
-    snake.size.pop()
-  }
-  render()
 }
--- Script/snake.js
+++ Script/snake.js
@@ -1,39 +1,85 @@
+/*----------exports/imports----------*/
+export {
+  changeDirection,
+  snakeMovement,
+  snake,
+}
 
+import {
+  win,
+  loss,
+  piece,
+  render,
+} from "./app.js"
+
+/*----------snake----------*/
 let snake= {
   direction: null,
+  nextDirection: null,
   size: ["8H"]
 }
 /*----------movement functions----------*/
 function up() {
-  console.log("up")
+  if (snake.direction !== "down") {
+    snake.nextDirection = "up"
+  }
 }
 function down() {
-  console.log("down")
+  if (snake.direction !== "up") {
+    snake.nextDirection = "down"
+  }
 }
 function left() {
-  console.log("left")
+  if (snake.direction !== "right") {
+    snake.nextDirection = "left"
+  }
 }
 function right() {
-  console.log("right")
+  if (snake.direction !== "left") {
+    snake.nextDirection = "right"
+  }
 }
-function changeDirection() {
-  // if (key === "ArrowUp") {
-  //   up()
-  // } else if (key === "ArrowDown") {
-  //   down()
-  // } else if (key === "ArrowLeft") {
-  //   left()
-  // } else if (key === "ArrowRight") {
-  //   right()
-  // }
-  console.log("sanity")
-}
-function exportCheck() {
-  console.log('sanity')
-}
-export {
-  changeDirection,
-  snake,
-  exportCheck
+function changeDirection(key) {
+  if (key === "ArrowUp") {
+    up()
+  } else if (key === "ArrowDown") {
+    down()
+  } else if (key === "ArrowLeft") {
+    left()
+  } else if (key === "ArrowRight") {
+    right()
+  }
 }
 
+function snakeMovement(){
+  if(loss === true || win === true) {
+    clearInterval(movementID)
+    return
+  }
+  snake.direction = snake.nextDirection
+  if (snake.direction === "up") {
+    let letter = snake.size[0].slice(-1)
+    let newLetter = String.fromCharCode(letter.codePointAt(0) - 1)
+    let newSquare = snake.size[0].replace(letter, newLetter)
+    snake.size.unshift(newSquare)
+  } else if (snake.direction === "down"){
+    let letter = snake.size[0].slice(-1)
+    let newLetter = String.fromCharCode(letter.codePointAt(0) + 1)
+    let newSquare = snake.size[0].replace(letter, newLetter)
+    snake.size.unshift(newSquare)
+  } else if (snake.direction === "left"){
+    let number = snake.size[0].slice(0,-1)
+    let newNumber = parseInt(number) - 1
+    let newSquare = snake.size[0].replace(number, newNumber)
+    snake.size.unshift(newSquare)
+  } else if (snake.direction === "right") {
+    let number = snake.size[0].slice(0,-1)
+    let newNumber = parseInt(number) + 1
+    let newSquare = snake.size[0].replace(number, newNumber)
+    snake.size.unshift(newSquare)
+  }
+  if ((snake.size[0] !== piece) && (snake.direction !== null)) {
+    snake.size.pop()
+  }
+  render()
+}
